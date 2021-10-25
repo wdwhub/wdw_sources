4fd3e0c1943bd6ac939bb4aff5fa5940e022199c
captproton <carl@wdwhub.net>
2021-10-25 09:38:34 -0700

    CHANGE: added more fields to attractions, dining venues, and hotels

--- db/migrate/20211024225455_create_wdw_sources_touringplans_hotels.rb
+++ db/migrate/20211024225455_create_wdw_sources_touringplans_hotels.rb
@@ -1,14 +1,36 @@
 class CreateWdwSourcesTouringplansHotels < ActiveRecord::Migration[6.1]
   def change
     create_table :wdw_sources_touringplans_hotels do |t|
-      t.string :name
-      t.string :short_name
-      t.string :sort_name
-      t.string :permalink
-      t.string :category_code
-      t.string :venue_permalink
+    t.string "name"
+    t.string "short_name"
+    t.string "permalink"
+    t.string "category_code"
+    t.string "venue_permalink"
+    t.string "address"
+    t.string "city"
+    t.string "state_code"
+    t.string "zip_code"
+    t.string "phone_number"
+    t.string "url"
+    t.boolean "off_site"
+    t.boolean "water_sports"
+    t.boolean "marina"
+    t.boolean "beach"
+    t.boolean "tennis"
+    t.boolean "biking"
+    t.boolean "suites"
+    t.boolean "concierge_floor"
+    t.boolean "room_service"
+    t.boolean "wired_internet"
+    t.boolean "wireless_internet"
+    t.integer "num_rooms"
+    t.string "theme"
+    t.string "cost_range"
+    t.boolean "shuttle_to_parks"
+    t.string "cost_estimate"
+    t.string "lodging_area_code"
 
-      t.timestamps
+    t.timestamps
     end
   end
 end
